a91a6d903b2f08407984d8108bc020b75019bfd7
Khaleel Jageer <jskcse4@gmail.com>
2020-04-20 01:47:05 +0530

    python3 migration

--- mailer.py
+++ mailer.py
@@ -4,20 +4,20 @@ import hashlib
 import sys
 import os
 import smtplib
-import urllib2
-from urlparse import urlparse
-from optparse import OptionParser
-from flask import Flask, request, redirect, url_for, render_template, session
-from email.MIMEMultipart import MIMEMultipart
-from email.MIMEBase import MIMEBase
-from email import encoders
+import urllib.request
 import time
 import datetime
+from urllib.parse import urlparse
+from optparse import OptionParser
+from flask import Flask, request, redirect, url_for, render_template, session
+from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
+from email import encoders
+from importlib import reload
 
-
-
-reload(sys)
-sys.setdefaultencoding("utf-8")
+if sys.version[0] == '2':
+    reload(sys)
+    sys.setdefaultencoding("utf-8")
 
 app = Flask(__name__)
 app.secret_key = hashlib.sha1(os.urandom(128)).hexdigest()
@@ -46,8 +46,8 @@ def mailer(email, file_url, file_name):
     """Function that downloads and emails the file."""
 
     ts = time.time()
-    timestamp = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d-%H-%M-%S')
-
+    timestamp = datetime.datetime.fromtimestamp(
+        ts).strftime('%Y-%m-%d-%H-%M-%S')
 
     fromaddr = "freetamilebooksteam@gmail.com"
     toaddr = email
@@ -59,14 +59,14 @@ def mailer(email, file_url, file_name):
 #    msg['cc'] = "tshrinivasan@gmail.com"
     part = MIMEBase('application', 'octet-stream')
     parsed_uri = urlparse(file_url)
-    if parsed_uri.hostname == 'freetamilebooks.com':	
-        
-        req = urllib2.Request(
-        file_url,
-        headers={
-            'User-Agent':
-            'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11'
-        })
+    if parsed_uri.hostname == 'freetamilebooks.com':
+
+        req = urllib.request.Request(
+            file_url,
+            headers={
+                'User-Agent':
+                'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11'
+            })
 
 #    file_name = file_name.encode('utf-8')
 
@@ -74,26 +74,27 @@ def mailer(email, file_url, file_name):
 #    print file_name
 #    os.system(" wget -O " + file_name + "   "  + file_url)
 
-        part.set_payload(urllib2.urlopen(req).read())
+        part.set_payload(urllib.request.urlopen(req).read())
 #    part.set_payload(open(file_name).read())
         encoders.encode_base64(part)
         part.add_header('Content-Disposition',
-#                    "attachment; filename= %s" % os.path.basename(file_name))
-                     "attachment; filename= %s" % file_name)
+                        #                    "attachment; filename= %s" % os.path.basename(file_name))
+                        "attachment; filename= %s" % file_name)
         msg.attach(part)
         server = smtplib.SMTP('smtp.gmail.com', 587)
         server.ehlo()
         server.starttls()
-        server.ehlo() 
+        server.ehlo()
         server.login(fromaddr, password)
         text = msg.as_string()
         server.sendmail(fromaddr, toaddr, text)
         server.quit()
 
         log = open("/var/www/html/Send2Kindle/logs/log.csv", "a")
-        log_content = timestamp +"," + email + "," + file_url +"," + file_name + "\n"
+        log_content = timestamp + "," + email + "," + file_url + "," + file_name + "\n"
         log.write(log_content)
         log.close()
+
 
 if __name__ == "__main__":
     parser = OptionParser()
